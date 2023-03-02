e18c85120cebe71a23a71d66e938dac534ce3f6b
Chris Sousa <chris.sousa@omegaenergia.com.br>
2023-03-02 12:37:48 -0300

    Appropriate Render when isInContainer

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+type ContainerWindowType = typeof window & { isInContainer: boolean };
+const isInContainer = (window as ContainerWindowType).isInContainer;
+
+const rootComponent = isInContainer ? 'hosted-app' : 'root'
+
+ReactDOM.createRoot(document.getElementById(rootComponent) as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
