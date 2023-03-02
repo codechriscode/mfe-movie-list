4c8e98a364584b92a3bd7ca37d0686ed5f2a466d
Chris Sousa <chris.sousa@omegaenergia.com.br>
2023-03-02 13:10:49 -0300

    Attribution adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ const TMDB_BASE_URL =
   "https://api.themoviedb.org/3/discover/movie?language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&api_key=";
 
 import { useEffect, useState } from "react";
-import attribution from "./assets/tmdb.svg";
 import "./App.css";
 
 function App() {
@@ -30,13 +29,15 @@ function App() {
       <div className="banner">
         <h1>Aqui está sua lista de filmes</h1>
         <div className="movie-list">
-          {featureList?.results?.length
-            ? featureList.results.map((m: any) => <h2>{m.original_title}</h2>)
-            : <h2>Carregando filmes populares...</h2>}
+          {featureList?.results?.length ? (
+            featureList.results.map((m: any) => <h2>{m.original_title}</h2>)
+          ) : (
+            <h2>Carregando filmes populares...</h2>
+          )}
         </div>
         <img
           className="attribution"
-          src={attribution}
+          src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg"
           alt="The Movie Database"
         />
       </div>
